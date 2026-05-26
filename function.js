d67703ab2e19f6181051aaf4438e42df1c965360
Drewflores <andrewflores303641@gmail.com>
2026-05-26 14:06:52 +0800

    Added a dialog in html

--- function.js
+++ function.js
@@ -1,23 +1,27 @@
 const myLibrary = [
     {
+        id: crypto.randomUUID(),
         title: "Lord Of Mysteries",
         author: "Cuttlefish That Loves Diving",
         genre: "Dark fantasy, cosmic horror",
         pages: 10000
     },
     {
+        id: crypto.randomUUID(),
         title: "Classroom of the Elite",
         author: "Shougo Kinugasa",
         genre: "psychological thriller",
         pages: 9500
     },
     {
+        id: crypto.randomUUID(),
         title: "Classic Literature Club",
         author: "Honobu Yonezawa",
         genre: "Mystery, Slice of life",
         pages: 2000
     },
     {
+        id: crypto.randomUUID(),
         title: "Noli Me Tangere",
         author: "Dr. Jose Rizal",
         genre: "Social, Political, Romance",
@@ -26,6 +30,7 @@ const myLibrary = [
 ];
 
 function Book(title, author, genre, pages){
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.genre = genre;
